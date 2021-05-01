c63d9f74db3fe6e763ab7641070d0b0de70c94ac
taletski <kirill.taletski@gmail.com>
2021-05-01 13:25:49 +0300

    initial commit

---
+++ src/services/CellsCreation/CellsCreationService.ts
@@ -0,0 +1,28 @@
+import {
+  ICellsCreationService,
+  UpdateStateCallback,
+} from "./CellsCreationService.types";
+
+export class CellsCreationService implements ICellsCreationService {
+  private static instance: CellsCreationService | null = null;
+  // private cellsQueue -- queue to store cells
+
+  private constructor() {}
+
+  private generateRandomCell() {}
+
+  private addCell() {}
+
+  public static getInstance() {
+    if (!CellsCreationService.instance) {
+      CellsCreationService.instance = new CellsCreationService();
+    }
+    return CellsCreationService.instance;
+  }
+
+  public async createAddCell() {}
+
+  public subscribeOnUpdates(callback: UpdateStateCallback) {}
+
+  public unsubscribeFromUpdates(callback: UpdateStateCallback) {}
+}
