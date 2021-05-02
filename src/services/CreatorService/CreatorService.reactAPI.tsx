4a9ca5d1af705fd97f855764d463b79d2b82a955
taletski <kirill.taletski@gmail.com>
2021-05-02 21:21:45 +0300

    refactor(): removes redundant handlers

--- src/services/CreatorService/CreatorService.reactAPI.tsx
+++ src/services/CreatorService/CreatorService.reactAPI.tsx
@@ -3,20 +3,13 @@ import { CreatorService } from "./CreatorService";
 import { CreatorState } from "./CreatorService.type";
 
 interface ICreatorServiceContext {
-  currentCreatorState: CreatorState | (() => never);
-  createAddRandomElement: () => Promise<void> | (() => never);
+  currentCreatorState: CreatorState | null;
+  createAddRandomElement: (() => Promise<void>) | null;
 }
 
-const CONTEXT_ERROR_MESSAGE =
-  "fatal: tried to access CreatorServiceContext fields or methods outside of the context provider";
-
 const defaultContext = {
-  currentCreatorState: () => {
-    throw new Error(CONTEXT_ERROR_MESSAGE);
-  },
-  createAddRandomElement: () => {
-    throw new Error(CONTEXT_ERROR_MESSAGE);
-  },
+  currentCreatorState: null,
+  createAddRandomElement: null,
 };
 
 const CreatorServiceContext = React.createContext<ICreatorServiceContext>(
