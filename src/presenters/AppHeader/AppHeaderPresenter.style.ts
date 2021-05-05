a45811d591aedbdcfe42581e033f107d372d45b6
taletski <kirill.taletski@gmail.com>
2021-05-05 23:19:08 +0300

    fix: fixes header styles

--- src/presenters/AppHeader/AppHeaderPresenter.style.ts
+++ src/presenters/AppHeader/AppHeaderPresenter.style.ts
@@ -4,10 +4,12 @@ export const S = {
   MainHeader: styled.h1`
     font-size: 20px;
     line-height: 28px;
+    font-weight: 500;
     letter-spacing: 0.0015em;
     color: ${({ theme }) => theme.header.colors.text};
 
     width: 100%;
     height: 100%;
+    text-align: center;
   `,
 };
